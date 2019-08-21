f5f44c6cfd66e134a3ae620b7aa51f7096a1ca1b
Md.Mehedi Hasan <35430592+mehedi6892@users.noreply.github.com>
2019-08-21 22:05:48 +0600

    Create PointerAdd.C

---
+++ PointerAdd.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+    int num1, num2, sum;
+
+    int *ptr1, *ptr2;
+
+    ptr1=&num1;
+    ptr2=&num2;
+
+    scanf("%d%d", ptr1, ptr2);
+
+    sum= *ptr1+ *ptr2;
+
+    printf("%d",sum);
+
+    return 0;
+}
